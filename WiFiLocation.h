dd185ff940cfb0da3d9ae291e2c8d7a57648e6ba
gk9339 <dev.9339@hotmail.com>
2020-03-09 12:30:50 +0800

    Formatting / layout changes

---
+++ WiFiLocation.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "data_structures.h" //Array<> type
+
+#define MAX_NETWORKS 50
+#define MAX_SAMPLES 50
+
+Array<String, MAX_NETWORKS> known_networks("");
---
+++ dataset_networks.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "WiFiLocation.h"
+
+void add_dataset_networks()
+{
+    known_networks.push("A4:91:B1:45:F1:F9");
+    known_networks.push("16:4F:8A:51:90:A5");
+    known_networks.push("80:3F:5D:55:EA:DC");
+    known_networks.push("B2:48:1A:B4:9E:EB");
+    known_networks.push("FA:BB:2C:D1:0C:85");
+}
